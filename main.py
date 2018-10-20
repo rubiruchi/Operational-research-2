6524f0070cb62940d187d68c0f19fd3953f87cca
Budziaszek <32570679+Budziaszek@users.noreply.github.com>
2018-10-20 18:42:45 +0200

    Prepared for loading by actions

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import wx.lib.scrolledpanel as scrolled
 from process_file import parse_line_into_queue
 
 
-class DataElement:
+class DataElementEvents:
     def __init__(self):
         self.activity = None
         self.time = 0
         self.first_event = None
         self.second_event = None
-
 
 class MainFrame(wx.Frame):
 
@@ -26,9 +25,9 @@ class MainFrame(wx.Frame):
         self.vbox = wx.BoxSizer(wx.VERTICAL)
 
         # Mode selection
-        # self.tbtn = wx.ToggleButton(panel, -1, "Change to event mode")
-        # vbox.Add(self.tbtn, 0, wx.EXPAND | wx.ALIGN_CENTER)
-        # self.tbtn.Bind(wx.EVT_TOGGLEBUTTON, self.OnToggle)
+        self.tbtn = wx.ToggleButton(self.panel, -1, "Event mode")
+        self.vbox.Add(self.tbtn, 0, wx.EXPAND | wx.ALIGN_CENTER)
+        self.tbtn.Bind(wx.EVT_TOGGLEBUTTON, self.on_toggle)
 
         # Title
         self.st = wx.StaticText(self.panel, label="Enter data")
@@ -38,7 +37,7 @@ class MainFrame(wx.Frame):
         self.st.SetFont(font)
         self.vbox.Add(self.st, 0, wx.ALIGN_CENTER)
 
-        # Labels
+        # Labels events
         hbox_labels = wx.BoxSizer(wx.HORIZONTAL)
         activity_label = wx.StaticText(self.panel, -1, label="Activity", size=(80, -1))
         time_label = wx.StaticText(self.panel, -1, label="Time", size=(80, -1))
@@ -50,25 +49,65 @@ class MainFrame(wx.Frame):
         hbox_labels.Add(add_label, 0, wx.ALIGN_RIGHT | wx.ALL, 5)
         self.vbox.Add(hbox_labels)
 
-        # Input
+        # Input events
         hbox_input = wx.BoxSizer(wx.HORIZONTAL)
-        self.activity_input = wx.TextCtrl(self.panel, -1, size=(80, -1))
-        self.time_input = wx.TextCtrl(self.panel, -1, size=(80, -1))
-        self.A_input = wx.TextCtrl(self.panel, -1, size=(50, -1))
-        self.B_input = wx.TextCtrl(self.panel, -1, size=(50, -1))
-        hbox_input.Add(self.activity_input, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        hbox_input.Add(self.time_input, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        hbox_input.Add(self.A_input, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        self.btn_add_e = wx.Button(self.panel, -1, "+", size=(30, -1))
+        self.activity_input_e = wx.TextCtrl(self.panel, -1, size=(80, -1))
+        self.time_input_e = wx.TextCtrl(self.panel, -1, size=(80, -1))
+        self.A_input_e = wx.TextCtrl(self.panel, -1, size=(50, -1))
+        self.B_input_e = wx.TextCtrl(self.panel, -1, size=(50, -1))
+        hbox_input.Add(self.activity_input_e, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_input.Add(self.time_input_e, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_input.Add(self.A_input_e, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
         hbox_input.Add(wx.StaticText(self.panel, -1, "->"), 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        hbox_input.Add(self.B_input, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-
-        # Buttons
-        self.btn_add = wx.Button(self.panel, -1, "+", size=(30, -1))
-        hbox_input.Add(self.btn_add, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        self.btn_add.Bind(wx.EVT_BUTTON, self.on_add_clicked)
+        hbox_input.Add(self.B_input_e, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_input.Add(self.btn_add_e, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
         self.vbox.Add(hbox_input)
 
+        self.input_event = []
+        self.input_event.append(self.activity_input_e)
+        self.input_event.append(self.time_input_e)
+        self.input_event.append(self.A_input_e)
+        self.input_event.append(self.B_input_e)
+
+        # Labels actions
+        hbox_labels = wx.BoxSizer(wx.HORIZONTAL)
+        activity_label = wx.StaticText(self.panel, -1, label="Activity", size=(80, -1))
+        time_label = wx.StaticText(self.panel, -1, label="Time", size=(80, -1))
+        succession_label = wx.StaticText(self.panel, -1, "Previous activity", size=(135, -1))
+        add_label = wx.StaticText(self.panel, -1, "Add", size=(30, -1))
+        hbox_labels.Add(activity_label, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_labels.Add(time_label, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_labels.Add(succession_label, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_labels.Add(add_label, 0, wx.ALIGN_RIGHT | wx.ALL, 5)
+        self.vbox.Add(hbox_labels)
+
+        # Input actions
+        hbox_input = wx.BoxSizer(wx.HORIZONTAL)
+        self.btn_add_a = wx.Button(self.panel, -1, "+", size=(30, -1))
+        self.activity_input_a = wx.TextCtrl(self.panel, -1, size=(80, -1))
+        self.time_input_a = wx.TextCtrl(self.panel, -1, size=(80, -1))
+        self.input_a = wx.TextCtrl(self.panel, -1, size=(133, -1))
+        hbox_input.Add(self.activity_input_a, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_input.Add(self.time_input_a, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_input.Add(self.input_a, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox_input.Add(self.btn_add_a, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        self.vbox.Add(hbox_input)
+        self.input_action = []
+        self.input_action.append(self.activity_input_a)
+        self.input_action.append(self.time_input_a)
+        self.input_action.append(self.input_a)
+
+        for inp in self.input_action:
+            inp.SetEditable(False)
+            inp.SetBackgroundColour((50, 50, 50))
+
+        # Buttons
+        self.btn_add_e.Bind(wx.EVT_BUTTON, self.on_add_by_events_clicked)
+        self.btn_add_a.Bind(wx.EVT_BUTTON, self.on_add_by_actions_clicked)
+
         self.data = []
+        self.is_loaded_as_events = True
 
         self.make_menu_bar()
         self.panel.SetSizer(self.vbox)
@@ -86,11 +125,8 @@ class MainFrame(wx.Frame):
         clear_item = menu.Append(-1, "&Clean\tCtrl-C", "Clean the data")
 
         menu_file = wx.Menu()
-        load_events_item = menu_file.Append(-1, "&Load (events succession)\tCtrl-E",
-                                            "Load data as succession of events")
-        menu_file.AppendSeparator()
-        load_activities_item = menu_file.Append(-1, "&Load (preceding activities)\tCtrl-A",
-                                                "Load data as preceding activities")
+        load_events_item = menu_file.Append(-1, "&Load from file\tCtrl-EL",
+                                            "Load data from file")
 
         menu_bar = wx.MenuBar()
         menu_bar.Append(menu, "&Menu")
@@ -99,46 +135,78 @@ class MainFrame(wx.Frame):
         self.SetMenuBar(menu_bar)
 
         self.Bind(wx.EVT_MENU, self.on_plot_clicked, plot_item)
-        self.Bind(wx.EVT_MENU, self.on_load_events_clicked, load_events_item)
-        # self.Bind(wx.EVT_MENU, self.OnClearClicked, clear_item)
+        self.Bind(wx.EVT_MENU, self.on_load_clicked, load_events_item)
+        self.Bind(wx.EVT_MENU, self.on_clear_clicked, clear_item)
 
-    def on_load_events_clicked(self, event):
+    def on_clear_clicked(self, event):
+        self.data = []
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        hbox.Add(wx.StaticText(self.panel, -1, "Cleaned"), 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        self.vbox.Add(hbox)
 
-        with wx.FileDialog(self, "Choose your data file..", wildcard="Text file(*.txt)|*.txt",
-                           style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+        for inp in self.input_action:
+            inp.Clear()
+        for inp in self.input_event:
+            inp.Clear()
 
-            if fileDialog.ShowModal() == wx.ID_CANCEL:
-                return  # User clean
+        self.panel.SetupScrolling()
+        self.SetStatusText("Data cleaned")
+        self.panel.SetSizer(self.vbox)
+        self.panel.Layout()
 
-            # User upload
-            pathname = fileDialog.GetPath()
-            try:
-                with open(pathname, 'r') as file:
-                    imported_graph = parse_line_into_queue(file.readlines())
-                    for i in range(0, len(imported_graph), 4):  # data sets are in 4s
-                        new_data_element = DataElement()
-                        new_data_element.activity = imported_graph[i]
-                        new_data_element.time = imported_graph[i + 1]
-                        new_data_element.first_event = imported_graph[i + 2]
-                        new_data_element.second_event = imported_graph[i + 3]
-                        self.data.append(new_data_element)
-                        self.set_data_element(new_data_element)
-            except IOError:
-                wx.LogError("Cannot open file '%s'.")
+    def on_load_clicked(self, event):
 
-    def on_add_clicked(self, event):
-        new_data_element = DataElement()
-        if not self.activity_input.GetValue() \
-                or not self.time_input.GetValue() \
-                or not self.A_input.GetValue() \
-                or not self.B_input.GetValue():
-            wx.MessageBox("Enter all data!")
-            return
+        if self.is_loaded_as_events:
+            with wx.FileDialog(self, "Choose your data file..", wildcard="Text file(*.txt)|*.txt",
+                               style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+
+                if fileDialog.ShowModal() == wx.ID_CANCEL:
+                    return  # User clean
+
+                # User upload
+                pathname = fileDialog.GetPath()
+                try:
+                    with open(pathname, 'r') as file:
+                        imported_graph = parse_line_into_queue(file.readlines())
+                        for i in range(0, len(imported_graph), 4):  # data sets are in 4s
+                            new_data_element = DataElementEvents()
+                            new_data_element.activity = imported_graph[i]
+                            new_data_element.time = imported_graph[i + 1]
+                            new_data_element.first_event = imported_graph[i + 2]
+                            new_data_element.second_event = imported_graph[i + 3]
+                            self.data.append(new_data_element)
+                            self.set_data_element(new_data_element)
+                except IOError:
+                    wx.LogError("Cannot open file '%s'.")
+
+    def on_add_by_actions_clicked(self, event):
+        new_data_element = DataElementEvents()
+        for inp in self.input_action:
+            if not inp.GetValue():
+                wx.MessageBox("Enter all data!")
+                return
         try:
-            new_data_element.time = float(self.time_input.GetValue())
-            new_data_element.activity = self.activity_input.GetValue()
-            new_data_element.first_event = self.A_input.GetValue()
-            new_data_element.second_event = self.B_input.GetValue()
+            new_data_element.time = float(self.time_input_e.GetValue())
+            new_data_element.activity = self.activity_input_e.GetValue()
+            new_data_element.first_event = self.A_input_e.GetValue()
+            new_data_element.second_event = self.B_input_e.GetValue()
+            self.data.append(new_data_element)
+        except ValueError:
+            wx.MessageBox("Wrong time! Enter number")
+            return
+        self.set_data_element(new_data_element)
+
+    def on_add_by_events_clicked(self, event):
+        new_data_element = DataElementEvents()
+        for inp in self.input_event:
+            if not inp.GetValue():
+                wx.MessageBox("Enter all data!")
+                return
+        try:
+            new_data_element.time = float(self.time_input_e.GetValue())
+            new_data_element.activity = self.activity_input_e.GetValue()
+            new_data_element.first_event = self.A_input_e.GetValue()
+            new_data_element.second_event = self.B_input_e.GetValue()
             self.data.append(new_data_element)
         except ValueError:
             wx.MessageBox("Wrong time! Enter number")
@@ -148,21 +216,16 @@ class MainFrame(wx.Frame):
     def set_data_element(self, data_element):
         # Data element added
         hbox = wx.BoxSizer(wx.HORIZONTAL)
-        activity = wx.StaticText(self.panel, -1, str(data_element.activity))
-        time = wx.StaticText(self.panel, -1, str(data_element.time))
-        A = wx.StaticText(self.panel, -1, str(data_element.first_event))
-        B = wx.StaticText(self.panel, -1, str(data_element.second_event))
-        hbox.Add(activity, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        hbox.Add(time, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        hbox.Add(A, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        hbox.Add(wx.StaticText(self.panel, -1, "->"), 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        hbox.Add(B, 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        hbox.Add(wx.StaticText(self.panel, -1,
+                               str(data_element.activity) + " " + str(data_element.time) + " "
+                               + str(data_element.first_event) + "->" + str(data_element.second_event)),
+                 0, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
         self.vbox.Add(hbox)
 
-        self.activity_input.Clear()
-        self.time_input.Clear()
-        self.A_input.Clear()
-        self.B_input.Clear()
+        for inp in self.input_action:
+            inp.Clear()
+        for inp in self.input_event:
+            inp.Clear()
 
         self.panel.SetupScrolling()
         self.SetStatusText("Item added")
@@ -197,6 +260,9 @@ class MainFrame(wx.Frame):
             self.add_nodes_t1(G, node, critical_path)
 
     def on_plot_clicked(self, event):
+        if len(self.data) == 0:
+            return
+
         G = nx.DiGraph()
         for dataElement in self.data:
             G.add_edges_from([(dataElement.first_event, dataElement.second_event)], time=int(dataElement.time))
@@ -228,13 +294,28 @@ class MainFrame(wx.Frame):
 
     def on_toggle(self, event):
         state = event.GetEventObject().GetValue()
-
+        self.on_clear_clicked(None)
         if state:
-            event.GetEventObject().SetLabel("Change to event mode")
+            event.GetEventObject().SetLabel("Action mode")
+            self.is_loaded_as_events = False
+            for inp in self.input_action:
+                inp.SetEditable(False)
+                inp.SetBackgroundColour((255, 255, 255))
+            for inp in self.input_event:
+                inp.SetEditable(False)
+                inp.SetBackgroundColour((50, 50, 50))
         else:
-            event.GetEventObject().SetLabel("Change to action mode")
+            event.GetEventObject().SetLabel("Event mode")
+            self.is_loaded_as_events = True
+            for inp in self.input_event:
+                inp.SetEditable(False)
+                inp.SetBackgroundColour((255, 255, 255))
+            for inp in self.input_action:
+                inp.SetEditable(False)
+                inp.SetBackgroundColour((50, 50, 50))
 
-    def OnExit(self, event):
+
+    def on_exit(self, event):
         """Close the frame, terminating the application."""
         self.Close(True)
 
